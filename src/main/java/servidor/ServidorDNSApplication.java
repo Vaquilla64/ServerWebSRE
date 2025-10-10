cb26e81c4ae3ac490658c0965a100f700682f690
Anas El Hani Marouane <AELHAN@upv.edu.es>
2025-10-10 20:23:45 +0200

    Configuración de cliente y pruebas completadas en local

--- src/main/java/servidor/ServidorDNSApplication.java
+++ src/main/java/servidor/ServidorDNSApplication.java
@@ -10,11 +10,10 @@ import java.util.Map;
 public class ServidorDNSApplication {
 
     public static void main(String[] args) throws IOException {
-        int puerto = 8053; // Usa 53 si tienes permisos root
+        int puerto = 8053; // 
         DatagramSocket socket = new DatagramSocket(puerto);
         System.out.println("Servidor DNS iniciado en puerto " + puerto);
 
-        // Zona de dominios -> IPs (simulación de tabla DNS)
         Map<String, String> zonas = new HashMap<>();
         zonas.put("example.com", "93.184.216.34");
         zonas.put("localhost", "127.0.0.1");
@@ -29,7 +28,7 @@ public class ServidorDNSApplication {
             String dominio = new String(request.getData(), 0, request.getLength()).trim();
             System.out.println("Consulta recibida para: " + dominio);
 
-            String ip = zonas.getOrDefault(dominio, "0.0.0.0"); // respuesta por defecto
+            String ip = zonas.getOrDefault(dominio, "0.0.0.0"); 
             byte[] respuesta = ip.getBytes();
 
             DatagramPacket reply = new DatagramPacket(
@@ -44,4 +43,3 @@ public class ServidorDNSApplication {
         }
     }
 }
-
